149b7ae0896175f8df3ab24aa1aee3d663335452
ardelladean <61878045+deana00@users.noreply.github.com>
2020-03-21 13:40:28 +0700

    Create data-sorting.cpp
    buat fungsi prototype pilihan( )

---
+++ data-sorting.cpp
@@ -0,0 +1,22 @@
+/*  Program untuk mengurutkan bilangan dari terkecil ke terbesar 
+    maupun terbesar ke terkecil menggunakan pointer array dan pointer fungsi.
+    Sebagai tugas responsi pengganti pertemuan ke-4.
+    Start on Sat, March 21st 2020 at 7.14 am */
+
+#include <iostream>
+using namespace std;
+
+void pilihan();
+
+int main (){
+  pilihan();
+return 0;
+}
+
+void pilihan(){
+	cout << "Urutkan : 1. Bilangan terkecil ke terbesar" << endl
+		 << "          2. Bilangan terbesar ke terkecil" << endl
+		 << "          3. Nama A-Z (1 kata)" << endl
+		 << "          4. Nama Z-A (1 kata)" << endl
+		 << "          (pilih 1,2,3, atau 4 saja)";
+}
