2ebd2021e37c93cda24cf1eb06900c39d9fc6029
Viny Silvia <62374456+VinySilvia@users.noreply.github.com>
2020-03-23 11:30:27 +0700

    Update data-sorting.cpp

--- data-sorting.cpp
+++ data-sorting.cpp
@@ -10,6 +10,49 @@ void pilihan();
 
 int main (){
   pilihan();
+  int size, tmp;
+  	char pilih; 
+
+  	cout << "Masukkan pilihan = " ;
+  	cin >> pilih ;
+  	cout << endl << endl ;
+
+
+  	cout << "========== Program Mengurutkan Bilangan ==========" << endl << endl ;
+  	cout << "Ada berapa banyak bilangan = " ;
+  	cin >> size ;
+  	
+  	int *data = new int[size];
+
+  	if (pilih == '1'){
+	  	for (int i=0; i<size; i++){
+	  		cout << "Bilangan ke " << (i+1)<< " = " ;
+	  		cin >> data[i] ;
+		}
+		  
+		cout << "Bilangan sebelum diurutkan = " ;
+		for (int i=0; i<size; i++){
+			cout << data[i] << " " ;
+		}
+		cout << endl << endl ;
+		
+		for (int i=0; i<size; i++){
+			for (int j=i+1; j<=size; j++){
+				if (data[i]>data[j]){
+					
+					tmp = data[i];
+					data[i] = data[j];
+					data[j] = tmp;							//before :: data[i] = tmp;
+				}
+			}
+	  	}
+
+	  	cout << "Bilangan setelah diurutkan = ";
+	  	for (int i=0; i<size; i++){
+	  		cout << data[i] << " " ;
+	  	}
+	}
+	cout << endl;
 return 0;
 }
 
